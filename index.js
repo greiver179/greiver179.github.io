9ccd1e0ceb4008be47bdeec233f4cf65f9d2c4bf
greiver179 <te_hung@hotmail.com>
2018-11-16 11:35:49 +0100

    added chat

---
+++ index.js
@@ -0,0 +1,21 @@
+const app = require('express')()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+
+io.on('connection', socket => {
+  console.log('a user connected')
+  socket.on('chat message', msg => {
+    console.log('message: ' + msg)
+    io.emit('chat message', msg)
+  })
+})
+
+server.listen(4000, () => {
+  console.log('The server is running: http://localhost:4000')
+})
+
